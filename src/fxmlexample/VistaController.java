fb1689125cbc0050956eec40daca7b8e00a139fc
Guillermo Benitez Marquez <guillermo.benitezmarquez@gmail.com>
2022-10-20 11:13:15 +0200

    First version of LoginFXML in JavaFX

---
+++ src/fxmlexample/VistaController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package fxmlexample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author usu2dam
+ */
+public class VistaController implements Initializable {
+    
+    @FXML private Text actiontarget;
+    
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
